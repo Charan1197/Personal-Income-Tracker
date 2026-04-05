eb4d2a63251ff10513fb5964fe247e0ae9233527
Charan1197 <kolapallicharan@gmail.com>
2026-04-05 13:57:08 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import sqlite3
 con = sqlite3.connect("income.db")
 cur = con.cursor()
+
+cur.execute(
+    '''CREATE TABLE salary(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    month TEXT UNIQUE,
+    total_salary INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );'''
+)
+
