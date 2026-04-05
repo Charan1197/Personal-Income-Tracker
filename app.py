6d58184dae31193834b7cd61472fd2c29cd99426
Ssnet9 <ssnetworld9@gmail.com>
2026-04-05 21:17:48 +0530

    added sql commands to app.py

--- app.py
+++ app.py
@@ -3,11 +3,25 @@ con = sqlite3.connect("income.db")
 cur = con.cursor()
 
 cur.execute(
-    '''CREATE TABLE salary(
+    '''CREATE TABLE IF NOT EXISTS salary(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     month TEXT UNIQUE,
     total_salary INTEGER,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     );'''
 )
+
+cur.execute(
+    ''' INSERT INTO salary (id, month, total_salary) VALUES (4, "May", 5000)'''
+)
+con.commit()
+
+cur.execute("SELECT * FROM salary")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+con.close()
+
+# use sqlite viewer extension 
 
